cbb76f782b96d80e281f046558ab86dc463a7286
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2025-08-01 09:09:42 -0400

    A script to get latest versions of system components for kubernetes

---
+++ sysprep-node/get_versions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+KUBERNETES_VERSION=$(curl -s https://api.github.com/repos/kubernetes/kubernetes/releases/latest | jq -r '.tag_name | sub("^v"; "")')
+CONTAINERD_VERSION=$(curl -s https://api.github.com/repos/containerd/containerd/releases/latest | jq -r '.tag_name | sub("^v"; "")')
+RUNC_VERSION=$(curl -s https://api.github.com/repos/opencontainers/runc/releases/latest | jq -r '.tag_name | sub("^v"; "")')
+CNI_PLUGINS_VERSION=$(curl -s https://api.github.com/repos/containernetworking/plugins/releases/latest | jq -r '.tag_name | sub("^v"; "")')
+CRICTL_VERSION=$(curl -s https://api.github.com/repos/kubernetes-sigs/cri-tools/releases/latest | jq -r '.tag_name | sub("^v"; "")')
+CILIUM_CLI_VERSION=$(curl -s https://api.github.com/repos/cilium/cilium-cli/releases/latest | jq -r '.tag_name | sub("^v"; "")')
+
+cat <<eof
+kubernetes_version: $KUBERNETES_VERSION
+containerd_version: $CONTAINERD_VERSION
+runc_version: $RUNC_VERSION
+cni_plugins_version: $CNI_PLUGINS_VERSION
+crictl_version: $CRICTL_VERSION
+cilium_cli_version: $CILIUM_CLI_VERSION
+eof
